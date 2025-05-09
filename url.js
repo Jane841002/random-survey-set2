e58288ed3077d0a469fd3fe0f28b01a857fd76f2
Jane841002 <jane84102@gmail.com>
2025-05-09 10:39:53 +0800

    Add files via upload

---
+++ url.js
@@ -0,0 +1,15 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
+ * 2. 包起來的網址之間用逗號分隔
+ */
+
+
+const urls = [
+	'https://forms.gle/43VS9ahhJM8BxjNX7',
+	'https://forms.gle/J3GkEchn3VF55cd76',
+	'https://forms.gle/ocUtEr8DXMisFSdE9',
+	'https://forms.gle/GUjfbNoKrgfZxTFy9',
+];
